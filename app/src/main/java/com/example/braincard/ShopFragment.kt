90da68515cc4afb157fa6ef9d7bd02c15d6b2a12
sameonline <samueledisumma@gmail.com>
2023-09-04 18:09:41 +0200

    bottoni sistemati dello shop

--- app/src/main/java/com/example/braincard/ShopFragment.kt
+++ app/src/main/java/com/example/braincard/ShopFragment.kt
@@ -133,53 +133,6 @@ class ShopFragment : Fragment() {
         return binding.root
 
     }
-    fun generaBanner(): LinearLayout {
-        val banner = LinearLayout(requireContext())
-        banner.orientation = LinearLayout.HORIZONTAL
-
-        val verticalLayout = LinearLayout(requireContext())
-        verticalLayout.orientation = LinearLayout.VERTICAL
-        verticalLayout.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.MATCH_PARENT
-        )
-        val text1 = TextView(requireContext())
-        text1.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-        text1.textSize = 20f // 20sp
-        text1.gravity = Gravity.CENTER
-        text1.text = "Text 1"
-
-
-        val text2 = TextView(requireContext())
-        text2.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-        text2.textSize = 16f // 16sp
-        text2.gravity = Gravity.CENTER
-        text2.text = "Text 2"
-
-        val text3 = TextView(requireContext())
-        text3.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-        text3.textSize = 16f // 16sp
-        text3.gravity = Gravity.CENTER
-        text3.text = "Text 3"
-
-        verticalLayout.addView(text1)
-        verticalLayout.addView(text2)
-        verticalLayout.addView(text3)
-
-        //banner.addView(image)
-        banner.addView(verticalLayout)
-        return banner
-
-    }
 
 
 
