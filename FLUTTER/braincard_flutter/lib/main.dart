9a8e31fcd0dae3e9f110a6c2dca027190e73f846
Leonardo Bordoni <bordonileonardo@gmail.com>
2023-09-08 14:57:18 +0200

    upd

--- FLUTTER/braincard_flutter/lib/main.dart
+++ FLUTTER/braincard_flutter/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart' as sqflite_ffi;
 
 void main() {
-  sqflite_ffi.databaseFactory = sqflite_ffi.databaseFactoryFfi;
   runApp(const MyApp());
 }
 
